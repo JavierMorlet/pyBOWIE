95e4af4abfda17f629b4c041b1047fb164bb3e1e
JavierMorlet <83840161+JavierMorlet@users.noreply.github.com>
2024-11-26 15:21:35 -0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ with open(os.path.join(filename, 'README.md'), encoding='utf-8') as f:
     README = f.read()
 
 setup(
-    name='pyBOWIE',
-    version='1.0.2',
+    name='pybowie',
+    version='1.0.5',
     description='Bayesian optimization for constrained or unconstrained, continuous, discrete or mixed data problems',
     author='Javier Morlet-Espinosa, Antonio Flores-Tlacuahuac',
     author_email='a00833961@tec.mx',
@@ -21,10 +21,12 @@ setup(
         'sympy>=1.11.1',
         'pandas>=2.0.3',
         'GPy>=1.10.0',
+        'gpflow >= 2.9.2',
         'scipy>=1.10.1',
         'scikit-learn>=1.1.3',
         'properscoring>=0.1',
         'prince>=0.12.1',
+        'multiprocess >= 0.70.16',
         'matplotlib>=3.7.3'
         ]
     )
